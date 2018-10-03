59eab3c45b9913bd90f90a9f58f44f804a8c2883
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-10-03 09:07:04 -0500

    extra js work on extra_exercises.js

---
+++ js/extra_exercises.js
@@ -0,0 +1,25 @@
+"use strict";
+
+alert("Make a sentence!");
+var noun = prompt("What noun would you use?");
+var verb = prompt("What verb would you use?");
+var adjective = prompt("What adjective would you use?");
+var adverb = prompt("What adverb would you use?");
+
+alert("Do you " + verb + " your " + adjective + noun + adverb + "?");
+
+
+
+var first = prompt("What is your first number?");
+var second = prompt("What is your second number?");
+first = parseFloat(first);
+second = parseFloat(second);
+var add = (first + second);
+var subtract = (first - second);
+var multiply = (first * second);
+var divide = (first / second);
+
+alert(first + " + " + second + " = " + add + "\n" + first + " - " + second + " = " + subtract + "\n" + first + " * " + second + " = " + multiply + "\n" + first + " / " + second + " = " + divide);
+
+
+
