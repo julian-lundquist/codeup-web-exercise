bf4f67285eb5433559102a1bea980ccb806e100d
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-10-03 09:05:13 -0500

    updated external.js

--- js/external.js
+++ js/external.js
@@ -11,12 +11,12 @@ alert("Price per day is $3 ea movie");
 var mermaid = prompt("How long did you rent Little Mermaid for?");
 var brother = prompt("How long did you rent Brother Bear for?");
 var hercules = prompt("How long did you rent Hercules for?");
-mermaid = parseInt(mermaid);
-brother = parseInt(brother);
-hercules = parseInt(hercules);
+mermaid = parseFloat(mermaid);
+brother = parseFloat(brother);
+hercules = parseFloat(hercules);
 
 var totalCost = (mermaid + brother + hercules) * 3;
-alert("The total cost for all 3 movies based on your inputs is " + totalCost + ".");
+alert("The total cost for all 3 movies based on your inputs is $" + totalCost.toFixed(2) + ".");
 
 
 
@@ -27,13 +27,13 @@ var facebookRate = 350;
 var googleHours = prompt("How many hours did you work at Google this week?");
 var amazonHours = prompt("How many hours did you work at Amazon this week?");
 var facebookHours = prompt("How many hours did you work at Facebook this week?");
-googleHours = parseInt(googleHours);
-amazonHours = parseInt(amazonHours);
-facebookHours = parseInt(facebookHours);
+googleHours = parseFloat(googleHours);
+amazonHours = parseFloat(amazonHours);
+facebookHours = parseFloat(facebookHours);
 
 var totalPay = (googleRate * googleHours) + (amazonRate * amazonHours) + (facebookRate * facebookHours);
 var totalHours = (googleHours + amazonHours + facebookHours);
-alert("Your total pay for " + totalHours + " hours worked is " + totalPay + ".");
+alert("Your total pay for " + totalHours + " hours worked is $" + totalPay.toFixed(2) + ".");
 
 
 
