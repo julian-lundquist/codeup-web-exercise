54634350ce08e6356304db88dcb3037878fb9a17
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-10-09 12:27:18 -0500

    finished while.js

---
+++ js/while.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var num1 = 2;
+do {
+    console.log(num1);
+    num1 = num1 * 2;
+} while (num1 <= 65536);
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    var perSale = Math.floor(Math.random() * 5) + 1;
+
+    if (allCones >= perSale) {
+        allCones -= perSale;
+        console.log(perSale + ' cones sold, ' + (allCones) + ' to go!');
+        if (allCones === 0) {
+            console.log('Wow! You sold out all the cones fast');
+        }
+    }
+    else if (allCones < perSale) {
+        console.log('You have ' + allCones + ' left and cannot sell ' + perSale + ' cones.');
+    }
+} while (0 < allCones);
