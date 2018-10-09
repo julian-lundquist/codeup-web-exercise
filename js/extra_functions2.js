5fb967b7eb19a0c1bf1dd01247d6bba5e13df223
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-10-09 12:29:12 -0500

    small updates

--- js/extra_functions2.js
+++ js/extra_functions2.js
@@ -1,17 +1,23 @@
 "use strict";
 
 
-function spaceCount(input) {
-    var num = input.length;
-    var countIt = input.trim();
-    var num2 = countIt.length;
-    var whiteSpace = num - num2;
-    return whiteSpace;
-}
-
-console.log(spaceCount(prompt('What whitespace do you want to count?')));
+// function spaceCount(input) {
+//     var num = input.length;
+//     var countIt = input.trim();
+//     var num2 = countIt.length;
+//     var whiteSpace = num - num2;
+//     return whiteSpace;
+// }
+//
+// console.log(spaceCount(prompt('What whitespace do you want to count?')));
 
 
 function inputDel(str, str2) {
-    
+    str.slice(str2);
+
 }
+
+var firstStr = prompt('What is the first string?');
+var secondStr = prompt('What is the second string?');
+
+console.log(inputDel(firstStr, secondStr));
