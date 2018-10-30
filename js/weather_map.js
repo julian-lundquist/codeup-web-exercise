fe249f18550f85869047abfa2267d3570e700af9
L <LL@Ls-iMac.local>
2018-10-30 13:48:48 -0500

    js weather map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     "use strict";
 
 
@@ -35,6 +34,77 @@ $(document).ready(function() {
         console.log(result.min);
     });
 
+    // ------------------------------------------------------------
+    // CODE FOR MAP
+    // Initialize the platform object:
+    var platform = new H.service.Platform({
+        'app_id': 'gar5OjSbItTwwU72V8q7',
+        'app_code': 'ph309B4Y5CtozD1_gOgpfg'
+    });
+
+    // Obtain the default map types from the platform object
+    var maptypes = platform.createDefaultLayers();
+
+    // Instantiate (and display) a map object:
+    var map = new H.Map(
+        document.getElementById('mapContainer'),
+        maptypes.normal.map,
+        {
+            zoom: 10,
+            center: { lng: 13.4, lat: 52.51 }
+        });
+    // function moveMapToBerlin(map){
+    //     map.setCenter({lat:52.5159, lng:13.3777});
+    //     map.setZoom(14);
+    // }
+    // moveMapToBerlin();
+    function addMarkerToGroup(group, coordinate, html) {
+        var marker = new H.map.Marker(coordinate);
+        // add custom data to the marker
+        marker.setData(html);
+        group.addObject(marker);
+    }
+
+    addMarkerToGroup(group, {lng: newLng.value, lat: newLat.value});
+
+
+    function addDraggableMarker(map, behavior){
+
+        var marker = new H.map.Marker({lat:42.35805, lng:-71.0636});
+        // Ensure that the marker can receive drag events
+        marker.draggable = true;
+        map.addObject(marker);
+
+        // disable the default draggability of the underlying map
+        // when starting to drag a marker object:
+        map.addEventListener('dragstart', function(ev) {
+            var target = ev.target;
+            if (target instanceof H.map.Marker) {
+                behavior.disable();
+            }
+        }, false);
+
+
+        // re-enable the default draggability of the underlying map
+        // when dragging has completed
+        map.addEventListener('dragend', function(ev) {
+            var target = ev.target;
+            if (target instanceof mapsjs.map.Marker) {
+                behavior.enable();
+            }
+        }, false);
+
+        // Listen to the drag event and move the position of the marker
+        // as necessary
+        map.addEventListener('drag', function(ev) {
+            var target = ev.target,
+                pointer = ev.currentPointer;
+            if (target instanceof mapsjs.map.Marker) {
+                target.setPosition(map.screenToGeo(pointer.viewportX, pointer.viewportY));
+            }
+        }, false);
+    }
+
 
     // alternative way of writing GET request
     // $.get({
@@ -48,3 +118,5 @@ $(document).ready(function() {
     // });
 
 });
+
+marker.map();
